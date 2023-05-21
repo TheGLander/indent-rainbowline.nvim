597ab9200eebf67ea04740fc9a675f4eb9c4839d
Lubomir <lubomir.yudenko@gmail.com>
2023-05-21 13:37:42 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# indent-rainbowline.nvim
+
+This plugin provides a way to generate [indent-blankline.nvim](https://github.com/lukas-reineke/indent-blankline.nvim/) configurations with rainbow indents, akin to the the [indent-rainbow](https://marketplace.visualstudio.com/items?itemName=oderwat.indent-rainbow) Visual Studio Code extension.
+
+![](./screenshot.png)
+
+## Installation
+
+Unlike most Neovim plugins, this plugin just provides configuration for another plugin, and can't be initialized on its own. Instead, it must be listed as a dependency for indent-blankline and used when configuring it.
+
+### Lazy.nvim
+
+```lua
+{
+	"lukas-reineke/indent-blankline.nvim",
+	opts = function(_, opts)
+		-- Other blankline configuration here
+		return require("indent-rainbowline").make_opts(opts)
+	end,
+	dependencies = {
+		"TheGLander/indent-rainbowline.nvim",
+	},
+}
+```
+
+### Packer.nvim
+
+(This configuration has not been tested. Please submit an issue if it doesn't work)
+
+```lua
+use {
+	"lukas-reineke/indent-blankline.nvim",
+	config = function()
+		opts = {}
+		-- Other blankline configuration here
+		return require("indent-rainbowline").make_opts(opts)
+	end,
+	requires = {
+		"TheGLander/indent-rainbowline.nvim",
+	},
+}
+```
+
+## Usage
+
+To customize how the rainbow indentation, pass a second argument to `make_opts`.
+
+```lua
+return require("indent-rainbowline").make_opts(opts, {
+	-- How transparent should the rainbow colors be. 1 is completely opaque, 0 is invisible. 0.07 by default
+	color_transparency = 0.15,
+	-- The 24-bit colors to mix with the background. Specified in hex.
+	-- { 0xffff40, 0x79ff79, 0xff79ff, 0x4fecec, } by default
+	colors = { 0xff0000, 0x00ff00, 0x0000ff, },
+})
+```
---
+++ lua/indent-rainbowline/init.lua
@@ -0,0 +1,86 @@
+local function mix_colors(a, b, mult)
+	return {
+		a[1] * mult + b[1] * (1 - mult),
+		a[2] * mult + b[1] * (1 - mult),
+		a[3] * mult + b[3] * (1 - mult),
+	}
+end
+
+local function decompose_color(color)
+	local blue = color % 256
+	local green = (color - blue) / 256 % 256
+	local red = (color - green * 256 - blue) / 256 / 256
+	return { red, green, blue }
+end
+
+local function compose_color(color)
+	return math.floor(math.floor(color[3]) + math.floor(color[2]) * 256 + math.floor(color[1]) * 256 * 256)
+end
+
+-- Generates a list of highlight groups based on the base highlight, but with some colors mixed in
+local function make_hl_groups(opts)
+	local color_transparency = opts.color_transparency or 0.07
+	local rainbow_colors = opts.colors or { 0xffff40, 0x79ff79, 0xff79ff, 0x4fecec }
+	local base_hl = opts.hl
+	local groups_name_prefix = opts.prefix
+	local color_groups = {}
+	local base_bg = decompose_color(base_hl.bg)
+	for i, rainbow_color_composed in ipairs(rainbow_colors) do
+		local rainbow_color = decompose_color(rainbow_color_composed)
+		local mixed_color = mix_colors(rainbow_color, base_bg, color_transparency)
+		local group_name = groups_name_prefix .. i
+		vim.api.nvim_set_hl(0, group_name, { bg = compose_color(mixed_color), fg = base_hl.fg })
+		table.insert(color_groups, group_name)
+	end
+	return color_groups
+end
+
+local function fill_missing_hl_parts(hl, base)
+	if hl.bg == nil then
+		hl.bg = base.bg
+	end
+	if hl.fg == nil then
+		hl.fg = base.fg
+	end
+end
+
+-- A slightly more terse way of getting a highlight from the Nvim API
+local function get_hl(name)
+	return vim.api.nvim_get_hl(0, { name = name, link = false })
+end
+
+-- Resolves the name by looking up the hightlight and defaulting to the normal fg/bg colors if they aren't set
+local function resolve_hl(name)
+	local hl = get_hl(name)
+	fill_missing_hl_parts(hl, get_hl("Normal"))
+	return hl
+end
+
+-- Mutate the given indent_blankline options to have rainbow space characters
+local function make_opts(blank_opts, rainbow_opts)
+	blank_opts = blank_opts or {}
+	rainbow_opts = rainbow_opts or {}
+	local hl = resolve_hl("IndentBlanklineChar")
+	local hl_context = resolve_hl("IndentBlanklineContextChar")
+	local hl_colors = make_hl_groups({
+		colors = rainbow_opts.colors,
+		color_transparency = rainbow_opts.color_transparency,
+		hl = hl,
+		prefix = "RainbowColor",
+	})
+	local hl_context_colors = make_hl_groups({
+		colors = rainbow_opts.colors,
+		color_transparency = rainbow_opts.color_transparency,
+		hl = hl_context,
+		prefix = "RainbowColorContext",
+	})
+	blank_opts.char_highlight_list = hl_colors
+	blank_opts.space_char_highlight_list = hl_colors
+	blank_opts.context_highlight_list = hl_context_colors
+	return blank_opts
+end
+
+return {
+	make_opts = make_opts,
+	make_hl_groups = make_hl_groups,
+}
